558d126853baff3c1a0411fdad144612f86769a1
Sensei Tarzan <gabay5155@gmail.com>
2022-06-29 14:36:14 +0200

    fix nether spawn ore

--- lib/generator/nether/populators/NetherOrePopulator.cpp
+++ lib/generator/nether/populators/NetherOrePopulator.cpp
@@ -4,7 +4,7 @@
 NetherOrePopulator::NetherOrePopulator(int_fast32_t worldHeight) {
   AddOre(OreType{NETHER_QUARTZ_ORE, 10, worldHeight - (10 * (worldHeight >> 7)), 13, static_cast<int>(NETHERRACK.GetId()), 16});
   AddOre(OreType{MAGMA, 26, 32 + (5 * (worldHeight >> 7)), 32, static_cast<int>(NETHERRACK.GetId()), 16});
-  AddOre(OreType{GOLD_ORE_NETHER, 10, worldHeight - (10 * (worldHeight >> 7)), 13, static_cast<int>(NETHERRACK.GetId()), 8});
-  AddOre(OreType{ANCIENT_DEBRIS, 10, worldHeight - (10 * (worldHeight >> 7)), 13, static_cast<int>(NETHERRACK.GetId()), 2});
+  AddOre(OreType{GOLD_ORE_NETHER, 10, worldHeight - (10 * (worldHeight >> 7)), 5, static_cast<int>(NETHERRACK.GetId()), 8});
+  AddOre(OreType{ANCIENT_DEBRIS, 10, worldHeight - (10 * (worldHeight >> 7)), 2, static_cast<int>(NETHERRACK.GetId())});
 
 }
