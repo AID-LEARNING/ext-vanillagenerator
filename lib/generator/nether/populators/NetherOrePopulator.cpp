d7353a046807c3d515f24de673912235d2d2fafc
Sensei Tarzan <gabay5155@gmail.com>
2022-06-29 14:40:09 +0200

    fix nether spawn ore

--- lib/generator/nether/populators/NetherOrePopulator.cpp
+++ lib/generator/nether/populators/NetherOrePopulator.cpp
@@ -2,9 +2,9 @@
 #include "NetherOrePopulator.h"
 
 NetherOrePopulator::NetherOrePopulator(int_fast32_t worldHeight) {
-  AddOre(OreType{NETHER_QUARTZ_ORE, 10, worldHeight - (10 * (worldHeight >> 7)), 13, static_cast<int>(NETHERRACK.GetId()), 16});
-  AddOre(OreType{MAGMA, 26, 32 + (5 * (worldHeight >> 7)), 32, static_cast<int>(NETHERRACK.GetId()), 16});
-  AddOre(OreType{GOLD_ORE_NETHER, 10, worldHeight - (10 * (worldHeight >> 7)), 5, static_cast<int>(NETHERRACK.GetId()), 8});
-  AddOre(OreType{ANCIENT_DEBRIS, 10, worldHeight - (10 * (worldHeight >> 7)), 2, static_cast<int>(NETHERRACK.GetId())});
+  AddOre(OreType{NETHER_QUARTZ_ORE, 10, worldHeight - (10 * (worldHeight >> 7)), 13, 16, static_cast<int>(NETHERRACK.GetId())});
+  AddOre(OreType{MAGMA, 26, 32 + (5 * (worldHeight >> 7)), 32, 16, static_cast<int>(NETHERRACK.GetId())});
+  AddOre(OreType{GOLD_ORE_NETHER, 10, worldHeight - (10 * (worldHeight >> 7)), 5, 5, static_cast<int>(NETHERRACK.GetId())});
+  AddOre(OreType{ANCIENT_DEBRIS, 10, worldHeight - (10 * (worldHeight >> 7)), 2, 8, static_cast<int>(NETHERRACK.GetId())});
 
 }
